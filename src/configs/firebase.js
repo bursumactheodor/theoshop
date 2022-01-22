5b67195e6372b8948175fcadd2a538a05c1f3a08
bursumactheodor <theodorbur@yahoo.com>
2022-01-22 19:44:12 +0200

    gitignore

--- src/configs/firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAaG8HzWWWe-m81bIjcKum6pOG7_4U5l0c",
-    authDomain: "theo-shop-test-eb004.firebaseapp.com",
-    projectId: "theo-shop-test-eb004",
-    storageBucket: "theo-shop-test-eb004.appspot.com",
-    messagingSenderId: "715392099283",
-    appId: "1:715392099283:web:49e1fd953a4391f204c526"
-  };
-export default firebaseConfig;
